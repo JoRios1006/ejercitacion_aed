617ad8f00f9b31d18c9a55dfd6f82bfe7985f4bb
Jo Rios <87106227+JoRios1006@users.noreply.github.com>
2026-04-10 03:34:19 -0300

    Refactor sys_write and sys_exit functions

--- heron/C/heron.c
+++ heron/C/heron.c
@@ -1,88 +1,76 @@
-#include <stddef.h>
-#include <stdint.h>
-
 #define SYS_WRITE 1
-#define SYS_EXIT 60
-#define STDOUT 1
-#define EPSILON 0.000000000000001
+#define SYS_EXIT  60
+#define STDOUT    1
+#define EPSILON   0.000000000000001
+#define TARGET    2.0
 
-static inline void wprint(const char *str, size_t len) {
-  asm volatile(
-      "mov %0, %%rax; mov %1, %%rdi; mov %2, %%rsi; mov %3, %%rdx; syscall" ::
-          "i"(SYS_WRITE),
-      "g"(STDOUT), "g"(str), "g"(len)
-      : "rax", "rdi", "rsi", "rdx");
+static inline void sys_write(const char *s, unsigned long len) {
+    asm volatile(
+        "mov %0, %%rax\n"
+        "mov %1, %%rdi\n"
+        "mov %2, %%rsi\n"
+        "mov %3, %%rdx\n"
+        "syscall"
+        :: "i"(SYS_WRITE), "i"(STDOUT), "g"(s), "g"(len)
+        : "rax", "rdi", "rsi", "rdx");
 }
 
-static inline void wexit() {
-  asm volatile("mov %0, %%rax;mov $0, %%rdi; syscall" ::"g"(SYS_EXIT)
-               : "rax", "rdi");
+static inline void sys_exit(void) {
+    asm volatile(
+        "mov %0, %%rax\n"
+        "mov $0, %%rdi\n"
+        "syscall"
+        :: "i"(SYS_EXIT) : "rax", "rdi");
 }
 
 static inline double _abs(double n) {
-  union {
-    double d;
-    uint64_t u;
-  } converter;
-  converter.d = n;
-  converter.u &= 0x7FFFFFFFFFFFFFFF;
-  return converter.d;
+    union { double d; unsigned long u; } c;
+    c.d = n;
+    c.u &= 0x7FFFFFFFFFFFFFFF;
+    return c.d;
 }
 
-uint8_t ram_block[128];
-#define GUESS    *(double *)((char *)ram + 0)
-#define LEN      *(int *)((char *)ram + 8)
-#define PRINT_BUF ((char *)ram + 12)
-static inline int double_to_str(double n, char *buf) {
-  int idx = 0;
-  if (n < 0) {
-    buf[idx++] = '-';
-    n = -n;
-  }
-  uint64_t int_part  = (uint64_t)n;
-  double   frac      = n - (double)int_part;
-  char tmp[20];
-  int  tlen = 0;
-  if (int_part == 0) {
-    tmp[tlen++] = '0';
-  } else {
-    uint64_t v = int_part;
-    while (v > 0) {
-      tmp[tlen++] = '0' + (v % 10);
-      v /= 10;
+static inline int ftoa(double x, char *out) {
+    char *start = out;
+    if (x < 0) { *out++ = '-'; x = -x; }
+
+    unsigned long i = (unsigned long)x;
+    double        f = x - (double)i;
+
+    char rev[20], *p = rev;
+    do { *p++ = '0' + (i % 10); i /= 10; } while (i);
+    while (p > rev) *out++ = *--p;
+
+    *out++ = '.';
+    for (int d = 0; d < 15; d++) {
+        f *= 10.0;
+        int digit = (int)f;
+        *out++ = '0' + digit;
+        f -= digit;
     }
-    for (int a = 0, b = tlen - 1; a < b; a++, b--) {
-      char c = tmp[a]; tmp[a] = tmp[b]; tmp[b] = c;
-    }
-  }
-  for (int i = 0; i < tlen; i++) buf[idx++] = tmp[i];
-  buf[idx++] = '.';
-  for (int i = 0; i < 15; i++) {
-    frac *= 10.0;
-    int digit = (int)frac;
-    buf[idx++] = '0' + digit;
-    frac -= digit;
-  }
-  return idx;
+    return out - start;
 }
 
-#define TARGET 2
+#define GUESS (*(double *)((char *)__builtin_frame_address(0) - 128))
+#define BUF   ((char *)__builtin_frame_address(0) - 120)
+
 void _start() {
-void *ram = (void *)ram_block;
-  GUESS = TARGET / 2.0;
-LOOP:
-  GUESS = (GUESS + TARGET / GUESS) / 2.0;
-  if (_abs((TARGET / GUESS) - GUESS) > EPSILON * 2) {
-    goto LOOP;
-  }
+    GUESS = TARGET / 2.0;
 
- PRINT:  
-  wprint("ROOT OF ", 8);
-  LEN = double_to_str(TARGET, PRINT_BUF);
-  wprint(PRINT_BUF, LEN);
-  wprint("  ", 2);
-  LEN = double_to_str(GUESS, PRINT_BUF);
-  wprint(PRINT_BUF, LEN);
-  wprint("\n", 1);
-  wexit();
+CONVERGE:
+    GUESS = (GUESS + TARGET / GUESS) * 0.5;
+    if (_abs((TARGET / GUESS) - GUESS) > EPSILON * 2)
+        goto CONVERGE;
+
+PRINT: {
+    char *p = BUF;
+    __builtin_memcpy(p, "ROOT OF ", 8); p += 8;
+    p += ftoa(TARGET, p);
+    __builtin_memcpy(p, "  ", 2);       p += 2;
+    p += ftoa(GUESS, p);
+    *p++ = '\n';
+    sys_write(BUF, p - BUF);
+}
+
+    sys_exit();
 }
